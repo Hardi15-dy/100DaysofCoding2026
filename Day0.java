7f859dbe15b9c43ac8727ebbf63137f435ff14a6
HARDI <hh6680748@gmail.com>
2026-09-02 17:58:58 +0800

    Add Java program to calculate final grades
    Sombong dikit

---
+++ Day0.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class App {
+	public static void main(String[] args) {
+		Scanner in = new Scanner (System.in);
+		
+		int mtk,ddp,pkn;
+		
+		System.out.printf("Masukkan nilai MK mtk : ");
+		mtk = in.nextInt();
+		
+		System.out.printf("Masukkan nilai MK ddp : ");
+		ddp = in.nextInt();
+		
+		System.out.printf("Masukkan nilai MK pkn : ");
+		pkn = in.nextInt();
+		
+		double h = mtk * 30.0 / 100;
+		double h2 = ddp * 40.0 / 100;
+		double h3 = pkn * 30.0 / 100;
+		
+		double total = h + h2 + h3;
+		
+		System.out.printf("dengan mengambil%nnilai 30%% dari mtk%nnilai 40%% dari ddp%nnilai 30%% dari pkn ");
+		
+		System.out.printf("%n%n%nNilai akhir kamu adalah %.2f",total);
+	}
+}

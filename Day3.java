70ae0d386f9783db2e2391ebc72f8a0f49d97582
HARDI <hh6680748@gmail.com>
2026-09-04 08:10:00 +0800

    Create Day3.java
    Day3 escape sequence(saya menambahkan 6 escape secuence)

---
+++ Day3.java
@@ -0,0 +1,33 @@
+public class day3 {
+    /**
+     * @author HARDI
+     */
+    public static void main(String[] args) {   
+        //Day 3 - Escape Sequence
+        //Saya menambahkan 7 escape sequence di bawah ini
+
+        //INI NORMAL
+        System.out.println("Nama saya adalah HARDI");
+
+        //INI escape baris baru
+        System.out.println("Nama saya \nadalah HARDI");
+        
+        //INI escape tab
+        System.out.println("Nama saya \tadalah HARDI");
+        
+        //INI escape agar ada kutipan
+        System.out.println("Nama saya adalah \"HARDI\"");
+        
+        //INI escape agar ada backslash
+        System.out.println("Nama saya adalah \\HARDI");
+
+        //INI escape agar ada backspace
+        System.out.println("Nama saya adalah \bHARDI");
+
+        //INI escape agar ada form feed
+        System.out.println("Nama saya adalah \fHARDI");
+        
+        //INI escape agar ada carriage return atau kembali ke awal baris
+        System.out.println("Nama saya adalah \rHARDI");
+    }
+}

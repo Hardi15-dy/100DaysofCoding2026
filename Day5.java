5a261d437fa2d800632ad9b46b209ffa099c688b
HARDI <hh6680748@gmail.com>
2026-09-06 00:08:03 +0800

    Add Day5.java to demonstrate numeric data types
    This Java program demonstrates the use of float and double data types, including their minimum and maximum values.

---
+++ Day5.java
@@ -0,0 +1,20 @@
+public class day5 {
+    /**
+     * @author HARDI
+     */
+    public static void main(String[] args) {
+        //Day 5 - Tipe Data Numerik Bilangan Pecahan
+        float f= 3.14f;
+        double d= 3.14;
+
+        System.out.println("===== CONTOH BILANGAN NUMERIK PECAHAN =====");
+        System.out.println("Nilai float: "+f);
+        System.out.println("Nilai double: "+d);
+
+        System.out.println("");
+
+        System.out.println("===== Value MIN DAN MAX tipe data numerik bilangan pecahan  =====");
+        System.out.println("Contoh bilangan float dari "+Float.MIN_VALUE+" sampai "+Float.MAX_VALUE);
+        System.out.println("Contoh bilangan Double dari "+Double.MIN_VALUE+" sampai "+Double.MAX_VALUE);
+    }
+}

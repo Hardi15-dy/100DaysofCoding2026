6e960f0e1313fe38b56a22f50a4688384cbb79be
HARDI <hh6680748@gmail.com>
2026-09-08 06:01:41 +0800

    Create Day7.java with string data type example
    Add Day7 class to demonstrate string data type in Java.

---
+++ Day7.java
@@ -0,0 +1,14 @@
+public class day7 {
+    /**
+     * @author HARDI
+     */
+    public static void main(String[] args) {
+
+        // Day 7 pengenalan tipe data string
+        //tipe data string adalah tipe data yang berisi kumpulan karakter atau huruf, angka, simbol, dan spasi. tipe data string di java diwakili oleh class String. tipe data string dapat di deklarasikan dengan menggunakan tanda kutip ganda (" ")
+        String nama = "Hardi";
+        String nim = "D0226020";
+
+        System.out.printf("Nama saya adalah %s %nNim %S adalah %s",nama,nama,nim);
+    }
+}

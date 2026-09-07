2c5f6ad1b9e6f68ed12aa63f4a67a608dc689ec9
HARDI <hh6680748@gmail.com>
2026-09-07 14:43:46 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,20 @@
+public class day6 {
+    /**
+     * @author HARDI
+     */
+    public static void main(String[] args) {
+        char a = 'B';
+        char d = 'A';
+        boolean b = true;
+        boolean c = false;
+
+
+        
+        System.out.println("\npenjelasan type data Boolean, adalah type data yang isi atau value nya hanya ada 2 kemungkinan yaitu True dan False");
+        System.out.printf("contoh nya adalah :%s dan %s %n%n",b,c);
+
+
+        System.out.println("penjelasan type data char, adalah type data yang isi atau value nya hanya 1 huruf jika lebih dari 1 huruf maka akan error");
+        System.out.printf("Contoh nya adalah %s dan %s",a,d);
+    }
+}

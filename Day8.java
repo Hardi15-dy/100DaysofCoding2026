3d2746438f87d8d7d7d4478e504cb89ba44ab2b6
HARDI <hh6680748@gmail.com>
2026-09-09 00:01:12 +0800

    Add Day8.java with name update example

---
+++ Day8.java
@@ -0,0 +1,15 @@
+public class day8 {
+    public static void main(String[] args) {
+        // Deklarasi
+        String nama;
+
+        // inisialisasi
+        nama = "Hardyyyy";
+
+        System.out.printf("Nama saya sebelum update adalah %s %n", nama);
+
+        // update atau mengubah value dari variable nama
+        nama = "Nurul Utami";
+        System.out.printf("Nama saya setelah update adalah %s", nama);
+    }
+}

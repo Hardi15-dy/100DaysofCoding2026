a587dc5a9f241ca04a649d411a5513e041d92591
HARDI <hh6680748@gmail.com>
2026-09-10 06:03:35 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,12 @@
+public class day9 {
+    public static void main(String[] args) {
+        // Day 9 pengenalan tipe data final
+
+        
+        final String nama = "Hardi";
+        System.out.printf("Nama saya adalah %s", nama);
+
+        // Data final tidak bisa diubah, jika diubah akan error
+        // nama = "Utami";
+    }
+}

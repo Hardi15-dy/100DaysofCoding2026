0be48e001cdeb7c5373d19c6e54dcf3e3d840470
HARDI <hh6680748@gmail.com>
2026-09-11 00:49:18 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,12 @@
+public class day10 {
+    public static void main(String[] args) {
+        // pemahaman batas minimal dan maksimal tipe data
+
+        System.out.println("batas minimal dan maksimal tipe data byte adalah " + Byte.MIN_VALUE + " dan " + Byte.MAX_VALUE);
+        System.out.println("batas minimal dan maksimal tipe data short adalah " + Short.MIN_VALUE + " dan " + Short.MAX_VALUE);
+        System.out.println("batas minimal dan maksimal tipe data int adalah " + Integer.MIN_VALUE + " dan " + Integer.MAX_VALUE);
+        System.out.println("batas minimal dan maksimal tipe data long adalah " + Long.MIN_VALUE + " dan " + Long.MAX_VALUE);
+        System.out.println("batas minimal dan maksimal tipe data float adalah " + Float.MIN_VALUE + " dan " + Float.MAX_VALUE);
+        System.out.println("batas minimal dan maksimal tipe data double adalah " + Double.MIN_VALUE + " dan " + Double.MAX_VALUE);
+    }
+}

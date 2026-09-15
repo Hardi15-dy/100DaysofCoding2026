fe4009842ec0739c46d18abd17f4b7aa8bb90ed5
HARDI <hh6680748@gmail.com>
2026-09-15 19:13:29 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,14 @@
+public class day14 {
+    public static void main(String[] args) {
+
+        // operator aritmatika penjumlahan dan pengurangan
+        int a = 10;
+        int b = 20;
+
+        int hasilPenjumlahan = a + b;
+        int hasilPengurangan = b - a;
+
+        System.out.printf("Hasil penjumlahan dari %d + %d = %d%n", a, b, hasilPenjumlahan);
+        System.out.printf("Hasil pengurangan dari %d - %d = %d%n", b, a, hasilPengurangan);
+    }
+}

11e9dd5d30c3b7fce0bfbe2f2cbcefc03e4b0b94
HARDI <hh6680748@gmail.com>
2026-09-16 03:26:02 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,17 @@
+public class day15 {
+    public static void main(String[] args) {
+        
+        // operator aritmatika perkalian dan pembagian
+        /*
+        Dalam operator aritmatika perkalian dan pembagian, kita dapat menggunakan simbol * untuk perkalian dan simbol / untuk pembagian.
+        */
+        int a = 10;
+        int b = 20;
+
+        int hasilPerkalian = a * b;
+        int hasilPembagian = b / a;
+
+        System.out.printf("Hasil perkalian dari %d * %d = %d%n", a, b, hasilPerkalian);
+        System.out.printf("Hasil pembagian dari %d / %d = %d%n", b, a, hasilPembagian);
+    }
+}

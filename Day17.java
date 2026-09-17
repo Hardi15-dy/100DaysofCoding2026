20a99ebbda8433154df9183b11f88c2e08253a82
HARDI <hh6680748@gmail.com>
2026-09-18 00:05:02 +0800

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,20 @@
+public class day17 {
+	public static void main(String[] args) {
+		// Day 17
+		// operator penugasan
+
+
+
+		int a = 10;
+
+		System.out.println("Sebelum pakai operator penugasan a : "+a);
+		a += 20;
+		System.out.println("Sesudah pakai operator penugasan a + 20 : "+a);
+		a *= 2;
+		System.out.println("Sesudah pakai operator penugasan a * 2 : "+a);
+		a /= 2;
+		System.out.println("Sesudah pakai operator penugasan a / 2 : "+a);
+		a -= 10;
+		System.out.println("Sesudah pakai operator penugasan a - 10 : "+a);
+	}
+}

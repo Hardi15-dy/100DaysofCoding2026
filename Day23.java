e7d9bb3c0eb8a6d0ba6d6e07aca56e7947e7781f
HARDI <hh6680748@gmail.com>
2026-09-24 20:20:45 +0800

    Create Day23.java

---
+++ Day23.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class day23 {
+    public static void main(String[] args) {
+        //Day 23
+		//Mencari luas persegi
+		Scanner in = new Scanner(System.in);
+		
+		int sisi,luas;
+		
+		System.out.print("Masukkan sisi : ");
+		sisi = in.nextInt();
+		in.nextLine();
+		
+		//Rumus mencari luas lingkaran yaitu L = sisi × sisi
+		luas = sisi*sisi;
+		
+		System.out.printf("Jadi luas dari persegi%ndengan sisi %dcm adalah : %dcm",sisi,luas);
+    }
+}
